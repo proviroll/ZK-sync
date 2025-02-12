a79be1f580c0df3085d892995e58941e75bd64cc
aminechakr <chakrelllah@gmail.com>
2025-02-12 18:26:44 +0100

    change faucet amount for testing

--- init/1_init_environment.sh
+++ init/1_init_environment.sh
@@ -53,7 +53,7 @@ if [ -z "${START_CONTAINERS:-}" ]; then
 fi
 
 if [ -z "${FAUCET_AMOUNT_ETH:-}" ]; then
-	export FAUCET_AMOUNT_ETH=5
+	export FAUCET_AMOUNT_ETH=0.1
 fi
 
 if [ -z "${DEPLOY_ERC20:-}" ]; then
