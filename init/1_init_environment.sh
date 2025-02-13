00b1e7eb5b41907f7f5f5e50e67bd03d2e7d581d
aminechakr <chakrelllah@gmail.com>
2025-02-13 22:23:27 +0100

    test

--- init/1_init_environment.sh
+++ init/1_init_environment.sh
@@ -29,7 +29,7 @@ if [ -z "${SET_AS_DEFAULT:-}" ]; then
 fi
 
 if [ -z "${CHAIN_ID:-}" ]; then
-	export CHAIN_ID=271
+	export CHAIN_ID=53151462
 fi
 
 if [ -z "${L1_BATCH_COMMIT_DATA_GENERATOR_MODE:-}" ]; then
