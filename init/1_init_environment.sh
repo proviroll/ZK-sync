22e501fba98525317f611f472d48df5775e6719a
aminechakr <chakrelllah@gmail.com>
2025-02-13 12:14:08 +0100

    fix(ci): :bug: Attempt to solve crash logging, lower eth costs for testing

--- init/1_init_environment.sh
+++ init/1_init_environment.sh
@@ -53,7 +53,7 @@ if [ -z "${START_CONTAINERS:-}" ]; then
 fi
 
 if [ -z "${FAUCET_AMOUNT_ETH:-}" ]; then
-	export FAUCET_AMOUNT_ETH=0.1
+	export FAUCET_AMOUNT_ETH=0.001
 fi
 
 if [ -z "${DEPLOY_ERC20:-}" ]; then
