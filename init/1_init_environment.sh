aac2540b7afb524e0fcaa07d71a82a06135c8b1c
aminechakr <chakrelllah@gmail.com>
2025-02-25 15:34:44 +0000

    Update amount

--- init/1_init_environment.sh
+++ init/1_init_environment.sh
@@ -53,7 +53,7 @@ if [ -z "${START_CONTAINERS:-}" ]; then
 fi
 
 if [ -z "${FAUCET_AMOUNT_ETH:-}" ]; then
-	export FAUCET_AMOUNT_ETH=0.005
+	export FAUCET_AMOUNT_ETH=6
 fi
 
 if [ -z "${DEPLOY_ERC20:-}" ]; then
